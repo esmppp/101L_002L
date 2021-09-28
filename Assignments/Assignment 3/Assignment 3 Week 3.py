ba98bfa9cce4bf2455de64710c889cec43acf3b4
esmppp <esmppp@umsystem.edu>
2021-09-27 19:19:33 -0500

    Update Assignment 3 Week 3.py

--- Assignments/Assignment 3/Assignment 3 Week 3.py
+++ Assignments/Assignment 3/Assignment 3 Week 3.py
@@ -1,3 +1,15 @@
+########################################################################
+##
+## CS 101 Lab
+## Program #3
+## Name     Ethan Miler
+## Email    esmppp@umsystem.edu
+##
+## PROBLEM : Describe the problem
+##      The problem here is that we need to make a number guesser based off of user input of remainders when an unknown number is divided by 3, 5, and 7
+## ALGORITHM : 
+##      1. Write out the algorithm
+## 
 print("Welcome to the Flarsheim Guesser!")
 userInput = 'y'
 while userInput.upper() == 'Y':
@@ -39,3 +51,11 @@ while userInput.upper() == 'Y':
     userInput = input("Do you want to play again? Y to continue, N to quit  ==> ")
     while not(userInput.upper() == 'Y' or userInput.upper() == 'N'):
         userInput = input("Do you want to play again? Y to continue, N to quit  ==> ")
+    print()
+## ERROR HANDLING:
+##      Any Special Error handling to be noted.  Wager not less than 0. etc
+##      I couldn't find any errors while testing, but I did encounter the problem with 'Y' versus 'y'. I made the userInput only check for caps and force any input to be in caps
+## OTHER COMMENTS:
+##      Any special comments
+##      I loved the lab, had a fun time working on this since I honestly wasn't sure how the math would work out for it.
+########################################################################
