738ca69014d87ba4df9fe28b666e1e42f4e1f99d
esmppp <esmppp@umsystem.edu>
2021-11-15 19:08:34 -0600

    Assignment 11

---
+++ Assignments/Assignment 11/Assignment 11.py
@@ -0,0 +1,11 @@
+from Clock import Clock
+import time
+seconds = 0
+hours = int(input("What is the current hour ==> "))
+minutes = int(input("What is the current minute ==> "))
+seconds = int(input("What is the current second ==> "))
+myClock = Clock(hours, minutes, seconds)
+while True:
+    print(myClock)
+    myClock.tick()
+    time.sleep(1)
